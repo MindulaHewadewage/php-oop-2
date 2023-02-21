0609e6e3c3acfee4f280fb2f809365fe38c243fe
MindulaHewadewage <mindufernando@gmail.com>
2023-02-21 17:22:25 +0100

    setter e getter -dati iniziali

---
+++ category.php
@@ -0,0 +1,76 @@
+<?php
+
+class Category
+{
+    private $name;
+    private $icon;
+
+    public function __construct()
+    {
+        $this->setName($name);
+        $this->setIcon($icon);
+    }
+
+    // get the value of
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    // set the value of
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    // get the value of
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+    // set the value of
+
+    public function setIcon($icon)
+    {
+        $this->icon = $icon;
+        return $this;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // get the value of
+    // public function get()
+    // {
+    //     return $this->;
+    // }
+
+
+
+
+
+
+
+    //     // set the value of
+
+    //     @return self
+
+    //     public function set()
+    //     {
+    //         $this->
+    //         return $this->;
+    //     }
--- products.php
+++ products.php
@@ -1,20 +1,101 @@
 <?php
+require_once __DIR__ . '/category.php'
 
 class Products
 {
+    public $id;
     public $img;
     public $title;
     public $price;
-    public $icon;
-    public $item;
+    public $category;
 
 
-    public function __construct($img, $title, $price, $icon, $item)
+    public function __construct($id, $img, $title, $price,Category $category)
+    {
+        $this->setImg($img);
+        $this->setTitle($title);
+        $this->setPrice($price);
+        $this->setCategory($category);
+
+    }
+
+
+
+    // get the value of id
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // set the value of id
+
+    public function setId()
+    {
+        $this->id = uniqid();
+        return $this;
+    }
+
+
+
+    // get the value of img
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    // set the value of img
+
+    public function setImg($img)
     {
         $this->img = $img;
-        $this->title = $title;
-        $this->price = $price;
-        $this->icon = $icon;
-        $this->item = $item;
+        return $this;
     }
+
+
+
+
+    // get the value of title
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    // set the value of title
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+
+    // get the value of price
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    // set the value of price
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+
+    // get the value of
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    // set the value of
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+        return $this;
+    }
+
 }
